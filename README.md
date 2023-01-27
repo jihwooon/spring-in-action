f98ab00b86157b1cec19d629747c75f4550e9aa5
jihwooon <jihwooon@gmail.com>
2023-01-27 20:45:28 +0900

    first init

---
+++ README.md
@@ -0,0 +1,6 @@
+# spring-class
+스프링 공부 모음
+
+## Spring-DATA-JPA
+
+## Spring-Shop-JPA
---
+++ Spring-Data-JPA/src/main/java/study/datajpa/SpringDataJpaApplication.java
@@ -0,0 +1,29 @@
+package study.datajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class SpringDataJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataJpaApplication.class, args);
+    }
+
+    @Bean
+    public AuditorAware<String> auditorProvider() {
+        return new AuditorAware<String>() {
+            @Override
+            public Optional<String> getCurrentAuditor() {
+                return Optional.of(UUID.randomUUID().toString());
+            }
+        };
+    }
+}
