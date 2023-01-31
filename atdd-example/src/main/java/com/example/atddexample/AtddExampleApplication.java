ba8dad29b327527e854a981e4a3cd113c4098c95
jihwooon <jihwooon@gmail.com>
2023-01-31 15:46:39 +0900

    first init

---
+++ atdd-example/src/main/java/com/example/atddexample/AtddExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.atddexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtddExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtddExampleApplication.class, args);
+    }
+
+}
