0ef289fcc3e62f1d6346f8113c6b3b0179ab0ece
jihwooon <jihwooon@gmail.com>
2023-01-31 15:46:39 +0900

    first init

---
+++ atdd-example/src/test/java/com/example/atddexample/AtddExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.atddexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtddExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
