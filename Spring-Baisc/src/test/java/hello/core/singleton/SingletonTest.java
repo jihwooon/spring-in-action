5dd8cc526020ceb374918a8603a1ca2669acff84
jihwooon <jihwooon@gmail.com>
2023-07-10 13:05:19 +0900

    싱글톤 패턴을 적용해서 테스트를 하라
    싱글톤 패턴은 클래스의 인스턴스를 한번 생성하도록 보장하는
    디자인패턴입니다.
    
    객체를 2개 이상 호출 하지 못하게 함으로써 객체의 동일성 보장합니다.
    
    이 패턴을 적용하면 요청이 올때마다 객체를 새로 생성하는 것이 아니라
    만들어진 객체를 공유함으로써 효율적으로 사용할 수 있습니다.

--- Spring-Baisc/src/test/java/hello/core/singleton/SingletonTest.java
+++ Spring-Baisc/src/test/java/hello/core/singleton/SingletonTest.java
@@ -24,4 +24,20 @@ public class SingletonTest {
 
         assertThat(memberService1).isNotSameAs(memberService2);
     }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    public void singletonServiceTest() {
+
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+
+        assertThat(singletonService1).isSameAs(singletonService2);
+
+        singletonService1.login();
+    }
+
 }
