adfe33a48e478bcf15d317899c23889bcb10244f
jihwooon <jihwooon@gmail.com>
2023-07-11 14:36:49 +0900

    싱글톤의 문제점에 대한 테스트를 하라
    싱글톤 방식은 여러 클라이언트가 하나의 객체 인스턴스를 공유하기 때문에
    상태를 유지해서는 안된다.
    
    price필드에 공유되는 필드가 존재하고 여러 스레드가 있으면 특정
    클라이언트 값을 변경합니다.
    
    이로 인해 원하는 값은 10000원인데 20000원이 나오는 결과값을 갖게 됩니다.

---
+++ Spring-Baisc/src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -0,0 +1,38 @@
+package hello.core.singleton;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+class StatefulServiceTest {
+
+    @Test
+    public void statefulServicesSingleton() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(
+            TestConfig.class);
+
+        StatefulService statefulService1 = ac.getBean("statefulService", StatefulService.class);
+        StatefulService statefulService2 = ac.getBean("statefulService", StatefulService.class);
+
+        statefulService1.order("userA", 10000);
+        statefulService2.order("userB", 20000);
+
+        int price = statefulService1.getPrice();
+
+        System.out.println("price = " + price);
+
+        assertThat(statefulService1.getPrice()).isEqualTo(20000);
+    }
+
+    static class TestConfig {
+
+        @Bean
+        public StatefulService statefulService() {
+            return new StatefulService();
+        }
+    }
+
+
+}
