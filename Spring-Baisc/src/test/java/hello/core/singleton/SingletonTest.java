822e9920685d8e674a62b54bf996cdadc0264e00
jihwooon <jihwooon@gmail.com>
2023-07-11 14:26:36 +0900

    싱글톤 컨테이너를 사용하는 테스트 코드를 작성하라
    기존 싱글톤의 문제점을 스프링 컨테이너로 문제를 해결합니다.
    스프링 컨테이너는 싱글톤 패턴을 적용하지 않아도 싱글톤 컨테이너 역할을
    합니다.
    
    이로 인해 요청이 올 때 마다 객체를 생성하지 않고 공유해서 효율적으로
    재사용이 가능해집니다.

--- Spring-Baisc/src/test/java/hello/core/singleton/SingletonTest.java
+++ Spring-Baisc/src/test/java/hello/core/singleton/SingletonTest.java
@@ -6,6 +6,8 @@ import hello.core.AppConfig;
 import hello.core.member.MemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class SingletonTest {
 
@@ -40,4 +42,20 @@ public class SingletonTest {
         singletonService1.login();
     }
 
+    @Test
+    @DisplayName("싱글톤 컨테이너와 싱글톤")
+    public void springContainer() {
+
+        ApplicationContext ac = new AnnotationConfigApplicationContext(
+            AppConfig.class);
+
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        assertThat(memberService1).isSameAs(memberService2);
+
+    }
 }
