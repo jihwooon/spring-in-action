4febe9104a5948fd3275c3d24acea66055e707f7
jihwooon <jihwooon@gmail.com>
2024-10-10 10:27:36 +0900

    기존 atdd-example 프로젝트를 atdd-practice 프로젝트로 변경합니다.

--- atdd-example/src/main/java/com/example/atddexample/application/CreateOrderService.java
+++
@@ -1,7 +0,0 @@
-package com.example.atddexample.application;
-
-import com.example.atddexample.domain.PendingOrder;
-
-public interface CreateOrderService {
-    PendingOrder createPendingOrder(PendingOrderRequest request);
-}
