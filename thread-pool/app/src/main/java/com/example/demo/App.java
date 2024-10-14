a558809f128738cc1b40abc5552e248b6e5c7d6b
jihwooon <jihwooon@gmail.com>
2024-10-14 21:26:04 +0900

    새로운 Java 프로젝트 'thread-pool' 초기 설정
    이 커밋은 새로운 Java 프로젝트 'thread-pool'의 초기 설정을 반영합니다.
    
    주요 변경사항:
    1. 프로젝트에 필요한 기본 파일(.editorconfig, .gitattributes, .gitignore, README.md) 생성
    2. Gradle 빌드 도구 설정 파일(build.gradle.kts, settings.gradle.kts) 추가
    3. Gradle wrapper 파일(gradlew, gradlew.bat) 추가
    4. 프로젝트의 메인 클래스(App.java) 및 테스트 클래스(AppTest.java) 생성
    5. 프로젝트에 사용될 라이브러리 버전 정보(libs.versions.toml) 추가

---
+++ thread-pool/app/src/main/java/com/example/demo/App.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+
+public class App {
+    private static final ScheduledExecutorService scheduler =
+            Executors.newScheduledThreadPool(1);
+
+    public static void main(String[] args) {
+
+    }
+}
