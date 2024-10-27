51ce0615d8361ac8e0834a92b45ac27df12477bc
jihwooon <jihwooon@gmail.com>
2024-10-28 00:29:24 +0900

    Objects null 참조 검사하기
    null 참조 검사 기법을 활용하여 고전적 null check 방식을 Objects 클래스를
    활용하여 체크하도록 했습니다.

---
+++ java-coding-problems/app/src/test/java/com/example/demo/객체와불변성/CheckNullTest.java
@@ -0,0 +1,29 @@
+package com.example.demo.객체와불변성;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class CheckNullTest {
+
+    @Test
+    void checkIfisNull() {
+        CheckNull checkNull = new CheckNull();
+
+        assertThatThrownBy(
+                () -> checkNull.evenInteger(null)
+        ).hasMessage("null이 발생했습니다.");
+    }
+
+    @Test
+    void checkIfNotNull() {
+        List<Integer> numbers = Arrays.asList(1, 2, null, 4, null, 16, 7, null);
+
+        CheckNull checkNull = new CheckNull();
+
+        assertThat(checkNull.evenInteger(numbers)).isEqualTo(List.of(2, 4, 16));
+    }
+}
