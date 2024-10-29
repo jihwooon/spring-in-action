7a4cb80e47915f56e30874e54cd7771ec1fb057a
jihwooon <jihwooon@gmail.com>
2024-10-29 15:26:23 +0900

    거래 단계 관리 및 비즈니스 룰 엔진
    이 코드 커밋은 거래 단계를 관리하는 Stage 열거형 클래스를 도입했습니다.
    BusinessRuleEngine 의 기능을 테스트하는 새로운 테스트 코드를 추가했습니다. 이 테스트는 kissen (mock) 객체를 사용하지 않고 실제 객체를 사용하여 테스트를 진행하는 단위 테스트 (unit test) 입니다.

--- real-world-software-development/ch05/app/src/test/java/com/example/demo/BusinessRuleEngineTest.java
+++ real-world-software-development/ch05/app/src/test/java/com/example/demo/BusinessRuleEngineTest.java
@@ -39,4 +39,35 @@ class BusinessRuleEngineTest {
 
         assertThat(businessRuleEngine.count()).isEqualTo(1);
     }
+
+    @Test
+    void shouldAddActionWith() {
+        var env = new Facts();
+        env.addFact("stage", "LEAD");
+        env.addFact("amount", "15");
+
+        BusinessRuleEngine businessRuleEngine = new BusinessRuleEngine(env);
+
+        businessRuleEngine.addAction(facts -> {
+            var forecastedAmount = 0.0;
+            Stage dealStage = Stage.valueOf(facts.getFacts("stage"));
+            double amount = Double.parseDouble(facts.getFacts("amount"));
+
+            if (dealStage == Stage.LEAD) {
+                forecastedAmount = amount * 0.2;
+            } else if (dealStage == Stage.EVALUATING) {
+                forecastedAmount = amount * 0.5;
+            } else if (dealStage == Stage.INTERESTED) {
+                forecastedAmount = amount * 0.8;
+            } else if (dealStage == Stage.CLOSED) {
+                forecastedAmount = amount;
+            }
+
+            facts.addFact("forecastedAmount", String.valueOf(forecastedAmount));
+        });
+
+        businessRuleEngine.run();
+
+        assertThat(env.getFacts("forecastedAmount")).isEqualTo("3.0");
+    }
 }
