317c0e54d797dce78fa1b4ed222e7a561b3d0c2e
jihwooon <jihwooon@gmail.com>
2024-10-29 14:19:29 +0900

    테스트 코드를 작성하기 위한 라이브러리 추가하라
    - Mockito와 assertJ 라이브러리 추가

--- real-world-software-development/ch05/app/build.gradle.kts
+++ real-world-software-development/ch05/app/build.gradle.kts
@@ -9,10 +9,13 @@ repositories {
 }
 
 dependencies {
-    // Use JUnit Jupiter for testing.
     testImplementation(libs.junit.jupiter)
 
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    testImplementation("org.assertj:assertj-core:3.26.3")
+
+    testImplementation("org.mockito:mockito-core:5.14.2")
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
