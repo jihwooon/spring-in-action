82319e57a2a19fc385aeddb97764b00bb15bf562
jihwooon <jihwooon@gmail.com>
2024-11-29 14:59:47 +0900

    싱글톤 패턴을 생성하라
    - 동일한 객체를 보장하기 위해 싱글턴 패턴을 적용해서 객체를 1개만 보장하도록 구현했습니다.

---
+++ spring-basic/src/test/java/hello/core/singleton/SingletonTest.java
@@ -0,0 +1,31 @@
+package hello.core.singleton;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SingletonTest {
+
+    @Test
+    @DisplayName("두 객체를 호출하지만 동일한 객체가 아닌것을 테스트하라")
+    void pureContainer() {
+        AppConfig appConfig = new AppConfig();
+
+        MemberService memberService1 = appConfig.memberService();
+        MemberService memberService2 = appConfig.memberService();
+
+        assertThat(memberService1).isNotSameAs(memberService2);
+    }
+
+    @Test
+    @DisplayName("두 객체가 동일하면 싱글톤을 보장하라")
+    void printSingleton() {
+        SingletonService instance1 = SingletonService.getInstance();
+        SingletonService instance2 = SingletonService.getInstance();
+
+        assertThat(instance1).isSameAs(instance2);
+    }
+}
