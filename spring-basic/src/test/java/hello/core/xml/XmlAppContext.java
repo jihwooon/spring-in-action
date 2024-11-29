1914b1a05b3e5e5cdcbcd6197125ff157c8e9d15
jihwooon <jihwooon@gmail.com>
2024-11-29 11:54:44 +0900

    스프링 XML 설정하라
    - xml 기반으로 스프링 빈 등록하여 어노테이션 기반과 비슷하게 구성이 가능합니다.

---
+++ spring-basic/src/test/java/hello/core/xml/XmlAppContext.java
@@ -0,0 +1,21 @@
+package hello.core.xml;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import hello.core.member.MemberService;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class XmlAppContext {
+
+    @Test
+    void xmlAppContext() {
+        GenericXmlApplicationContext genericXmlApplicationContext = new GenericXmlApplicationContext(
+            "appConfig.xml");
+
+        MemberService memberService = genericXmlApplicationContext.getBean("memberService",
+            MemberService.class);
+
+        assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
