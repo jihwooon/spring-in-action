c00d2f7df009921cbc37acefebb5dd8ef58c7fc5
jihwooon <jihwooon@gmail.com>
2024-12-06 11:43:38 +0900

    빈 생명주기 어노테이션을 적용하라
    PostConstruct와 PreDestroy 으로 컨테이너 생명주기를 확인 할 수 있습니다. 다만 외부 라이브러리에 적용하지 못 합니다. 때문에 Bean 애노테이션으로 초기화, 종료를 직접 사용해야 합니다.

--- spring-basic/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ spring-basic/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,9 +1,9 @@
 package hello.core.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
 
     private String url;
 
@@ -27,14 +27,14 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close: " + url);
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
+    @PostConstruct
+    public void init() {
         connect();
         call("초기화 연결 메시지");
     }
 
-    @Override
-    public void destroy() throws Exception {
+    @PreDestroy
+    public void close() {
         disconnect();
     }
 }
