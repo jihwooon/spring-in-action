49e867fd43b71a6ca939e1338922d40292d614f7
jihwooon <jihwooon@gmail.com>
2024-12-28 13:46:04 +0900

    MyLogger 빈 주입 방식 변경 및 로그 출력 수정
    MyLogger 빈을 ObjectProvider를 통해 주입하고, 로그 출력 형식을 수정했습니다.
    
    주요 변경사항:
    1. MyLogger 클래스에서 Lombok의 @Setter 제거 및 setRequestURL 메서드 추가
    2. LogDemoController와 LogDemoService에서 MyLogger 빈을 ObjectProvider를 통해 주입
    3. 로그 출력 시 UUID가 제대로 출력되도록 수정
    4. LogDemoController의 logDemo 메서드에 Thread.sleep 추가

--- spring-basic/src/main/java/hello/core/config/MyLogger.java
+++ spring-basic/src/main/java/hello/core/config/MyLogger.java
@@ -3,19 +3,17 @@ package hello.core.config;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import java.util.UUID;
-import lombok.Setter;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
 @Scope(value = "request")
-@Setter
 public class MyLogger {
 
     private String uuid;
     private String requestURL;
 
-    public MyLogger(final String requestURL) {
+    public void setRequestURL(String requestURL) {
         this.requestURL = requestURL;
     }
 
@@ -24,13 +22,13 @@ public class MyLogger {
     }
 
     @PostConstruct
-    public void init () {
-        String uuid = UUID.randomUUID().toString();
-        System.out.println("[" + uuid +"] request scope bean create:" + this);
+    public void init() {
+        uuid = UUID.randomUUID().toString();
+        System.out.println("[" + uuid + "] request scope bean create:" + this);
     }
 
     @PreDestroy
     public void close() {
-        System.out.println("[" + uuid +"] request scope bean create:" + this);
+        System.out.println("[" + uuid + "] request scope bean create:" + this);
     }
 }
