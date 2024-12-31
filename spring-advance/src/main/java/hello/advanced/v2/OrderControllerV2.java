7f7eced549c13544cb907735f928df66742dfb99
jihwooon <jihwooon@gmail.com>
2024-12-31 17:15:01 +0900

    OrderControllerV2, OrderServiceV2, OrderRepositoryV2 추가
    OrderControllerV2, OrderServiceV2, OrderRepositoryV2 클래스를 추가하여 주문 처리 로직을 구현했습니다.
    
    주요 변경사항:
    1. OrderControllerV2 클래스 추가
       - /v2/request 엔드포인트를 통해 주문 요청을 처리
       - HelloTraceV2를 사용하여 트랜잭션 추적
    2. OrderServiceV2 클래스 추가
       - orderItem 메서드를 통해 주문 아이템 저장 로직 처리
       - HelloTraceV2를 사용하여 트랜잭션 추적
    3. OrderRepositoryV2 클래스 추가
       - save 메서드를 통해 아이템 저장 로직 구현
       - 예외 발생 시 트랜잭션 추적 및 예외 처리

---
+++ spring-advance/src/main/java/hello/advanced/v2/OrderControllerV2.java
@@ -0,0 +1,29 @@
+package hello.advanced.v2;
+
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.hellotrace.HelloTraceV2;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV2 {
+
+    private final OrderServiceV2 orderServiceV2;
+    private final HelloTraceV2 helloTraceV2;
+
+    @GetMapping("/v2/request")
+    public String request(String itemId) {
+        TraceStatus status = null;
+        try {
+            status = helloTraceV2.begin("OrderController.request()");
+            orderServiceV2.orderItem(status.getTraceId(), itemId);
+            helloTraceV2.end(status);
+            return "ok";
+        } catch (Exception e) {
+            helloTraceV2.exception(status, e);
+            throw e;
+        }
+    }
+}
